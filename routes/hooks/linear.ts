7cd1c2415996184083f117c781545270315fcf7b
AprilNEA <github@sku.moe>
2023-10-29 11:20:06 +0800

    fix: body already consume

--- routes/hooks/linear.ts
+++ routes/hooks/linear.ts
@@ -3,14 +3,19 @@ import { createHmac } from "https://deno.land/std@0.173.0/node/crypto.ts";
 import { sendMarkdown } from "../../libs/sender.ts";
 export const handler: Handlers = {
   async POST(req: Request, _ctx: HandlerContext) {
-    const signature = createHmac("sha256", Deno.env.get("WEBHOOK_SECRET")!)
-      .update(
-        await req.arrayBuffer(),
-      ).digest("hex");
+    const bodyBuffer = await req.arrayBuffer();
+    const bufferView = new Uint8Array(bodyBuffer);
+    const jsonView = new TextDecoder().decode(bufferView);
+    const signature = createHmac(
+      "sha256",
+      Deno.env.get("WEBHOOK_SECRET") ?? "1",
+    ).update(
+      bodyBuffer,
+    ).digest("hex");
     if (signature !== req.headers.get("linear-signature")) {
       return new Response(undefined, { status: 400 });
     }
-    const payload = await req.json();
+    const payload = JSON.parse(jsonView);
     // const { action, data, type, createdAt } = payload;
     await sendMarkdown(payload);
     // const event: "Issue" | "Comment" = req.headers.get("Linear-Event");
